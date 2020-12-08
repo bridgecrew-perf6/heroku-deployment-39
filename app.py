99a1f6215080151d289ef27351fcc28449259b11
Tristan <tristan15thompson@gmail.com>
2020-12-08 13:00:29 -0800

    [EDIT] double reference to pymongo

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, redirect, render_template, url_for
-from flask_pymongo import PyMongo
+from flask_pymongo import PyMongo, MongoClient
 from bson.objectid import ObjectId
-from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
 
